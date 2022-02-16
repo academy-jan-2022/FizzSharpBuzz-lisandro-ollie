6338387d51e0bcf92347af21f8cdd9d225e8bff9
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-16 16:37:22 +0100

    refactor

--- src/FPFizzBuzz/FPFizzBuzz/Library.fs
+++ src/FPFizzBuzz/FPFizzBuzz/Library.fs
@@ -7,13 +7,13 @@ let (|MULTIPLE_OF_THREE_AND_FIVE|MULTIPLE_OF_THREE|MULTIPLE_OF_FIVE|IS_NUM|) num
     | 0, 0 -> MULTIPLE_OF_THREE_AND_FIVE
     | 0, _ -> MULTIPLE_OF_FIVE
     | _, 0-> MULTIPLE_OF_THREE
-    | _ -> IS_NUM
+    | _ -> IS_NUM number
     
     
-let convert number =
-  match number with
+let convert =
+  function
   | MULTIPLE_OF_THREE_AND_FIVE -> "FizzBuzz"
   | MULTIPLE_OF_THREE -> "Fizz"
   | MULTIPLE_OF_FIVE -> "Buzz"
-  | IS_NUM -> number.ToString()
+  | IS_NUM number -> string number
 
