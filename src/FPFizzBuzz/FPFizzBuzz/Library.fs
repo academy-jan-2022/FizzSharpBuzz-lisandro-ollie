5c3922f36471e0955a075029138e9c85b6582c7d
oawood93 <oa.wood@outlook.com>
2022-02-16 15:03:50 +0000

    refactor

--- src/FPFizzBuzz/FPFizzBuzz/Library.fs
+++ src/FPFizzBuzz/FPFizzBuzz/Library.fs
@@ -1,9 +1,16 @@
 ﻿module FizzBuzz
 
+
+
+let (|IS_FIZZ|IS_BUZZ|IS_NUM|) number =
+    if number % 3 = 0 then IS_FIZZ
+    elif number % 5 = 0 then IS_BUZZ
+    else IS_NUM
+    
+    
 let convert number =
-  if number % 3 = 0
-  then "Fizz"
-  elif number % 5 = 0
-  then "Buzz"
-  
-  else number.ToString()
+  match number with
+  | IS_FIZZ -> "Fizz"
+  | IS_BUZZ -> "Buzz"
+  | IS_NUM -> number.ToString()
+
